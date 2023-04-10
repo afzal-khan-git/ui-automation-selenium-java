e86e43cceb1454bae1828fc843016f02075e61da
afzal-khan-git <afzalkhan2357@gmail.com>
2023-04-10 10:03:14 -0400

    add quit browser ater each test

--- src/test/java/com/ak/selenium/testbase/TestBase.java
+++ src/test/java/com/ak/selenium/testbase/TestBase.java
@@ -4,10 +4,7 @@ import com.ak.selenium.driverfactory.BrowserDriverFactory;
 import com.ak.selenium.driverfactory.BrowserDriverType;
 import com.ak.selenium.util.ConfigPropertyReaderUtil;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestBase {
 
@@ -16,9 +13,16 @@ public class TestBase {
     @BeforeTest
     @Parameters("browserName")
     public void launchBrowserAndNavigateToURL(@Optional("CHROME") String browserName){
-        driver = BrowserDriverFactory.getBrowserDriverInstance("FIREFOX");
+        driver = BrowserDriverFactory.getBrowserDriverInstance(browserName.toUpperCase());
         driver.navigate().to(ConfigPropertyReaderUtil.getPropertyValueByKey("app_url","app_test.properties"));
 
+    }
+
+    @AfterTest
+    public void tearDown(){
+        if (driver != null){
+            driver.quit();
+        }
     }
 
 
